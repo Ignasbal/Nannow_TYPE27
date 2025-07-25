66dc867f21a76d206e0c999c23ade82b2ce722a7
Ignasbal <ignaswind@gmail.com>
2025-07-25 20:09:45 +0300

    Added desktop header

---
+++ assets/assets.js
@@ -0,0 +1,51 @@
+import arrow_down from "./arrow_down.svg";
+import art from "./art.svg";
+import can_swim from "./can_swim.svg";
+import cooking from "./cooking.svg";
+import crown from "./crown.svg";
+import de from "./de.svg";
+import eng from "./eng.svg";
+import first_aid from "./first_aid.svg";
+import heart_outlined from "./heart_outlined.svg";
+import languages from "./languages.svg";
+import logo from "./logo.svg";
+import love_animals from "./love_animals.svg";
+import lt from "./lt.svg";
+import non_smoking from "./non_smoking.svg";
+import pl from "./pl.svg";
+import play_music from "./play_music.svg";
+import professional from "./professional.svg";
+import share from "./share.svg";
+import sport from "./sport.svg";
+import star_icon_1 from "./star_icon_1.svg";
+import star_icon_2 from "./star_icon_2.svg";
+import star_icon from "./star_icon.svg";
+import teacher from "./teacher.svg";
+import verify from "./verify.svg";
+
+export const assets = {
+  art,
+  arrow_down,
+  can_swim,
+  cooking,
+  crown,
+  de,
+  eng,
+  first_aid,
+  heart_outlined,
+  languages,
+  logo,
+  love_animals,
+  lt,
+  non_smoking,
+  pl,
+  play_music,
+  professional,
+  share,
+  sport,
+  star_icon_1,
+  star_icon_2,
+  star_icon,
+  teacher,
+  verify,
+};
---
+++ components/Header/Header.jsx
@@ -0,0 +1,76 @@
+import { assets } from "@/assets/assets";
+import styles from "./styles.module.css";
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className={styles.navigationBar}>
+      <div className={styles.headerWrapper}>
+        <header className={styles.header}>
+          <div className={styles.logoContainer}>
+            <Image
+              src={assets.logo}
+              alt="Website Logo"
+              className={styles.logo}
+            />
+          </div>
+
+          <nav className={styles.navWrapper}>
+            <ul className={styles.navLinks}>
+              <li>
+                <Link href="/#">Apie Nannow</Link>
+              </li>
+              <li>
+                <Link href="/#">Ieškau auklės</Link>
+              </li>
+              <li>
+                <Link href="/#">Noriu būti aukle</Link>
+              </li>
+              <li>
+                <Link href="/#">Kaip veikia</Link>
+              </li>
+              <li>
+                <Link href="/#">Savybės</Link>
+              </li>
+              <li>
+                <Link href="/#">Įkūrėjai</Link>
+              </li>
+              <li>
+                <Link href="/#">DUK</Link>
+              </li>
+            </ul>
+
+            <div className={styles.navigationButtons}>
+              <Link href="#" className={styles.registerButton}>
+                <h1>Registruokis</h1>
+              </Link>
+
+              <Link href="/#" className={styles.languageSelection}>
+                <h1>LT</h1>
+                <Image
+                  src={assets.arrow_down}
+                  className={styles.arrowDownIcon}
+                  alt="Arrow icon"
+                />
+              </Link>
+            </div>
+          </nav>
+
+          <button onClick={openSideMenu} className={styles.menuButton}>
+            <Image
+              src={assets.menu_icon}
+              alt="Menu Icon"
+              className={styles.menu_icon}
+            />
+          </button>
+
+          {/*------Mobile menu------ */}
+        </header>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
